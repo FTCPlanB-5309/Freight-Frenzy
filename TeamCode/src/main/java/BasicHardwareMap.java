0203713501036e5743cd927cb73d921bd41d3772
Tony Congdon <congdontony@yahoo.com>
2021-10-12 20:28:14 -0400

    Addition of balance wheels Removal of unused code

--- TeamCode/src/main/java/BasicHardwareMap.java
+++ TeamCode/src/main/java/BasicHardwareMap.java
@@ -1,68 +1,14 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-//import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
-import com.qualcomm.hardware.bosch.BNO055IMU;
-import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
-
-import org.firstinspires.ftc.robotcore.external.ClassFactory;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
-import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
-import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
 
 public class BasicHardwareMap
 {
-
-//    public final int ARM_UP = 2987;
-//    public final int ARM_DOWN = 0;
-
-    public DcMotor  leftFrontDrive  = null;
-    public DcMotor  rightFrontDrive = null;
-    public DcMotor  leftRearDrive  = null;
-    public DcMotor  rightRearDrive = null;
+    public DcMotor leftDrive = null;
+    public DcMotor rightDrive = null;
     public DcMotor  duckSpinner = null;
-//    public DcMotor armMotor = null;
 
-    public Servo armRotator = null;
-    public Servo armLifter = null;
-    public Servo leftGrabber = null;
-    public Servo rightGrabber = null;
     public Servo duckArm = null;
     public Servo frontWheel = null;
     public Servo rearWheel = null;
@@ -70,113 +16,68 @@ public class BasicHardwareMap
     HardwareMap hwMap = null;
 
     //Hardware constants
-
     public static final double TELEOPDEADZONE = 0.05;
-
-    public final double LEFT_GRABBER_OPEN = 0.0;
-    public final double RIGHT_GRABBER_OPEN = 0.5;
-
-    public final double LEFT_GRABBER_CLOSED = 0.5;
-    public final double RIGHT_GRABBER_CLOSED = 0;
 
     public static final double DUCK_ARM_OUT = 0;
     public static final double DUCK_ARM_IN = 0.33;
 
-    public static final double FRONT_WHEEL_DOWN = 0.33;
-    public static final double FRONT_WHEEL_UP = 0.5;
-    public static final double REAR_WHEEL_DOWN = 0.33;
-    public static final double REAR_WHEEL_UP = 0.5;
-
-
-
+    public static final double FRONT_WHEEL_DOWN = 0.18;
+    public static final double FRONT_WHEEL_UP = 0.05;
+    public static final double REAR_WHEEL_DOWN = .36;
+    public static final double REAR_WHEEL_UP = 0.23;
 
     public void teleopInit(HardwareMap ahwMap) {
         // Save reference to Hardware map
         hwMap = ahwMap;
 
 //         Define and Initialize Motors
-        leftFrontDrive  = hwMap.get(DcMotor.class, "leftFrontDrive");
-        rightFrontDrive = hwMap.get(DcMotor.class, "rightFrontDrive");
-        leftRearDrive  = hwMap.get(DcMotor.class, "leftRearDrive");
-        rightRearDrive = hwMap.get(DcMotor.class, "rightRearDrive");
+        leftDrive = hwMap.get(DcMotor.class, "leftDrive");
+        rightDrive = hwMap.get(DcMotor.class, "rightDrive");
         duckSpinner = hwMap.get(DcMotor.class, "duckSpinner");
 
-//        armMotor = hwMap.get(DcMotor.class, "armMotor");
-
-        armRotator = hwMap.get(Servo.class, "armRotator");
-        armLifter = hwMap.get(Servo.class, "armLifter");
-        leftGrabber = hwMap.get(Servo.class, "leftGrabber");
-        rightGrabber = hwMap.get(Servo.class, "rightGrabber");
         duckArm = hwMap.get(Servo.class, "duckArm");
         frontWheel = hwMap.get(Servo.class, "frontWheel");
         rearWheel = hwMap.get(Servo.class, "rearWheel");
 
 
-        leftFrontDrive.setDirection(DcMotorSimple.Direction.REVERSE);
-        leftRearDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftDrive.setDirection(DcMotorSimple.Direction.REVERSE);
 
         // Set servos to start positions
         duckArm.setPosition(DUCK_ARM_IN);
-        armLifter.setPosition(.5);
-        armRotator.setPosition(.5);
-        leftGrabber.setPosition(LEFT_GRABBER_OPEN);
-        rightGrabber.setPosition(RIGHT_GRABBER_OPEN);
         frontWheel.setPosition(FRONT_WHEEL_DOWN);
         rearWheel.setPosition(REAR_WHEEL_DOWN);
 
 //         Set all motors to zero power
-        leftFrontDrive.setPower(0);
-        rightFrontDrive.setPower(0);
-        leftRearDrive.setPower(0);
-        rightRearDrive.setPower(0);
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
         duckSpinner.setPower(0);
-//        armMotor.setPower(0);
-//
-//        armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
 //         Set all motors to run without encoders.
 //         May want to use RUN_USING_ENCODERS if encoders are installed.
-        leftFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rightFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        leftRearDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rightRearDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-//        armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-
+        leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
-    public void resetEncoder () {
-        leftFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        leftRearDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightRearDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
+    public void resetDriveEncoders () {
+        leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     }
 
-    public void setupDriveTrain () {
-        resetEncoder();
-
-        leftFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        leftRearDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        rightFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        rightRearDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    public void driveToPosition () {
+        resetDriveEncoders();
+        leftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
 
 
-    public void runUsingEncoder () {
-        resetEncoder();
-
-        leftFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        leftRearDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rightFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rightRearDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    public void driveUsingEncoders () {
+        resetDriveEncoders();
+        leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
     public void stop () {
-        leftFrontDrive.setPower(0);
-        rightFrontDrive.setPower(0);
-        leftRearDrive.setPower(0);
-        rightRearDrive.setPower(0);
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
     }
-
 }
--- TeamCode/src/main/java/TwoWheelTank_Teleop.java
+++ TeamCode/src/main/java/TwoWheelTank_Teleop.java
@@ -1,11 +1,6 @@
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.util.Range;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Gamepad;
-
 
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="Two Wheel Tank Drive", group="Teleop")
-
 
 public class TwoWheelTank_Teleop extends LinearOpMode {
 
@@ -15,15 +10,8 @@ public class TwoWheelTank_Teleop extends LinearOpMode {
     @Override
     public void runOpMode() {
 
-        double right_stick_x;
-        double right_stick_y;
-
-        double lift_rate;
-        double rotation_rate;
-
         double left;
         double right;
-
 
         robot.teleopInit(hardwareMap);
 
@@ -36,67 +24,34 @@ public class TwoWheelTank_Teleop extends LinearOpMode {
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
+//  Duck attachment code, WIP
+//
+//            if (gamepad2.a)
+//                robot.duckSpinner.setPower(1);
+//            else robot.duckSpinner.setPower(0);
+//
+//            if (gamepad2.dpad_right)
+//                robot.duckArm.setPosition(robot.DUCK_ARM_OUT);
+//
+//            if (gamepad2.dpad_left)
+//                robot.duckArm.setPosition(robot.DUCK_ARM_IN);
 
-
-
-
-            // Attachments
-
-
-            right_stick_x = gamepad2.right_stick_x;
-
-            if (Math.abs(right_stick_x) > robot.TELEOPDEADZONE) {
-                rotation_rate = Math.abs(right_stick_x) * .00126 + .000737;
-                if (right_stick_x > 0)
-                    robot.armRotator.setPosition(robot.armRotator.getPosition() + rotation_rate);
-                else
-                    robot.armRotator.setPosition(robot.armRotator.getPosition() - rotation_rate);
-            }
-
-            telemetry.addData("rotator position", robot.armRotator.getPosition());
-            telemetry.addData("lifter position", robot.armLifter.getPosition());
-            telemetry.update();
-
-
-            right_stick_y = gamepad2.right_stick_y;
-            if (Math.abs(right_stick_y) > robot.TELEOPDEADZONE) {
-                lift_rate = Math.abs(right_stick_y) * .001 + .000737;
-                if (right_stick_y > 0)
-                    robot.armLifter.setPosition(robot.armLifter.getPosition() + lift_rate);
-                else
-                    robot.armLifter.setPosition(robot.armLifter.getPosition() - lift_rate);
-
-            }
-
-            if (gamepad2.right_trigger > 0.8) {
-                robot.leftGrabber.setPosition(robot.LEFT_GRABBER_CLOSED);
-                robot.rightGrabber.setPosition(robot.RIGHT_GRABBER_CLOSED);
-            }
-
-            if (gamepad2.right_bumper) {
-                robot.leftGrabber.setPosition(robot.LEFT_GRABBER_OPEN);
-                robot.rightGrabber.setPosition(robot.RIGHT_GRABBER_OPEN);
-            }
-
-            if (gamepad2.a)
-                robot.duckSpinner.setPower(1);
-            else robot.duckSpinner.setPower(0);
-
-            if (gamepad2.dpad_right)
-                robot.duckArm.setPosition(robot.DUCK_ARM_OUT);
-
-            if (gamepad2.dpad_left)
-                robot.duckArm.setPosition(robot.DUCK_ARM_IN);
-
-
-            // DriveTrain
-
+            // Drivetrain
             left = -gamepad1.left_stick_y;
             right = -gamepad1.right_stick_y;
 
-            robot.leftFrontDrive.setPower(left);
-            robot.rightFrontDrive.setPower(right);
+            robot.leftDrive.setPower(left);
+            robot.rightDrive.setPower(right);
 
+            // Balance wheels
+            if(gamepad1.dpad_up) {
+                robot.frontWheel.setPosition(robot.FRONT_WHEEL_UP);
+                robot.rearWheel.setPosition(robot.REAR_WHEEL_UP);
+            }
+            if(gamepad1.dpad_down) {
+                robot.frontWheel.setPosition(robot.FRONT_WHEEL_DOWN);
+                robot.rearWheel.setPosition(robot.REAR_WHEEL_DOWN);
+            }
         }
     }
 }
