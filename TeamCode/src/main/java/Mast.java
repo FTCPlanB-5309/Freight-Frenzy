05109dcbf3cfe5058c8aef226797c425619b9a7d
DillonWgithub <dpwa1k3r@gmail.com>
2021-11-14 14:57:58 -0500

    added setmode and yield to mast

--- TeamCode/src/main/java/Mast.java
+++ TeamCode/src/main/java/Mast.java
@@ -1,4 +1,5 @@
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
@@ -20,20 +21,32 @@ public class Mast {
 
     public void left() {
         robot.mastRotator.setTargetPosition(leftPosition);
+        robot.mastRotator.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         robot.mastRotator.setPower(0.1);
-
+        while (robot.mastRotator.isBusy()) {
+            Thread.yield();
+        }
+        robot.mastRotator.setPower(0);
     }
 
     public void right() {
         robot.mastRotator.setTargetPosition(rightPosition);
+        robot.mastRotator.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         robot.mastRotator.setPower(0.1);
-
+        while (robot.mastRotator.isBusy()) {
+            Thread.yield();
+        }
+        robot.mastRotator.setPower(0);
     }
 
     public void forward() {
         robot.mastRotator.setTargetPosition(forwardPosition);
+        robot.mastRotator.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         robot.mastRotator.setPower(0.1);
-
+        while (robot.mastRotator.isBusy()) {
+            Thread.yield();
+        }
+        robot.mastRotator.setPower(0);
     }
 
 }
